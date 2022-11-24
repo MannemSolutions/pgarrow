823ea48a95bc09e4d602b66f9593a740a54d0267
Sebastiaan Mannem <sebas@mannem.nl>
2022-11-24 09:42:40 +0100

    Testing with pg11

--- config/schema.sql
+++ config/schema.sql
@@ -1,4 +1,9 @@
+create database src;
+\c src
 DROP PUBLICATION IF EXISTS pgarrow;
 CREATE PUBLICATION pgarrow FOR ALL TABLES;
 create table t (id int primary key, name text);
-insert into t values(coalesce((select max(id) from t1),0)+1, 'foo');
+insert into t values(coalesce((select max(id) from t),0)+1, 'foo');
+create database dest;
+\c dest
+create table t (id int primary key, name text);
