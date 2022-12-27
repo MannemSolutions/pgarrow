a6f0f9419ea09eb9d4952a5323b9a14498212640
Sebastiaan Mannem <sebas@mannem.nl>
2022-12-28 00:41:10 +0100

    Minor tweak in docker-compose-tests.sh

--- docker-compose-tests.sh
+++ docker-compose-tests.sh
@@ -14,8 +14,9 @@ for ((i=0;i<10;i++)); do
 done
 docker exec -u postgres ${COMPOSE_PROJDIR}-postgres-1 /usr/bin/psql -tc "select datname from pg_database where datname='src'" | grep -q src || docker exec -u postgres ${COMPOSE_PROJDIR}-postgres-1 /usr/bin/psql -c '\i /host/config/schema.sql'
 if [ "${ARROW_BIN_SOURCE}" = "github" ]; then
-  curl -L https://github.com/MannemSolutions/pgarrow/releases/download/$ARROW_BIN_VERSION/arrow-$ARROW_BIN_VERSION-linux-amd64.tar.gz | tar -C ./docker/arrow -xz --include arrow && mv ./docker/arrow/arrow ./docker/arrow/arrow.amd64
-  curl -L https://github.com/MannemSolutions/pgarrow/releases/download/$ARROW_BIN_VERSION/arrow-$ARROW_BIN_VERSION-linux-arm64.tar.gz | tar -C ./docker/arrow -xz --include arrow && mv ./docker/arrow/arrow ./docker/arrow/arrow.arm64
+  mkdir -p bin
+  curl -L https://github.com/MannemSolutions/pgarrow/releases/download/$ARROW_BIN_VERSION/arrow-$ARROW_BIN_VERSION-linux-amd64.tar.gz | tar -C ./bin -xz --include arrow && mv ./bin/arrow ./bin/arrow.amd64
+  curl -L https://github.com/MannemSolutions/pgarrow/releases/download/$ARROW_BIN_VERSION/arrow-$ARROW_BIN_VERSION-linux-arm64.tar.gz | tar -C ./bin -xz --include arrow && mv ./bin/arrow ./bin/arrow.arm64
   cp config/pgarrow.yml "./docker/arrow/"
 elif [ "${ARROW_BIN_SOURCE}" = "build" ]; then
   docker-compose up -d builder
